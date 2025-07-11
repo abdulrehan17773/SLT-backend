7bdb84ae216e07b297624a4ba0809699af9a18bf
abdulrehan17773 <theroyal.info4u@gmail.com>
2025-07-11 16:05:16 +0500

    feat: Add admin dashboard functionalities for user statistics and recent users

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { addUser, getAllUsers, updateUser, deleteUser } from "../controllers/admin.controllers.js";
+import { addUser, getAllUsers, updateUser, deleteUser, getUserCounts, getRecentUsers, getLast7DaysUsers, getLast4WeeksUsers } from "../controllers/admin.controllers.js";
 import {checkAdmin} from "../middlewares/checkAuth.middleware.js"
 
 const UserRouter = Router();
@@ -10,5 +10,9 @@ UserRouter.route("/addUser").post(addUser);
 UserRouter.route("/getAllUsers").get(getAllUsers);
 UserRouter.route("/updateUser").patch(updateUser);
 UserRouter.route("/deleteUser").patch(deleteUser);
+UserRouter.route("/getUserCounts").get(getUserCounts);
+UserRouter.route("/getRecentUsers").get(getRecentUsers);
+UserRouter.route("/getLast7DaysUsers").get(getLast7DaysUsers);
+UserRouter.route("/getLast4WeeksUsers").get(getLast4WeeksUsers);
 
 export { UserRouter };  
